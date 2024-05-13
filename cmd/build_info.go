e9dfd2b0a3ec5d1effb40c3fcc6bdc2b28b1f6d6
ziti-ci <ziti-ci@netfoundry.io>
2024-05-13 13:49:24 +0000

    Release v0.5.112

--- cmd/build_info.go
+++ cmd/build_info.go
@@ -20,9 +20,9 @@
 package cmd
 
 const (
-	Version   = "v0.3.11"
-	Revision  = "169c17d6f16f"
+	Version   = "v0.5.112"
+	Revision  = "015f110b696e"
 	Branch    = "v1"
-	BuildUser = "Paul Lorenz"
-	BuildDate = "2020-02-10 14:50:41"
+	BuildUser = ""
+	BuildDate = "2024-05-13T13:49:24Z"
 )
